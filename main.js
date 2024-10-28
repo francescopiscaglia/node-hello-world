ad878c948f6177ab581fda3c5dd0c6dab6c2c194
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-28 15:09:13 +0100

    add verification bonus

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ console.log(pin);
 
 const arg = process.argv[2]
 console.log(arg);
+
+let message = "";
+
+if (arg == "admin" && pin == 666) {
+    message = "Welcome adming";
+} else {
+    message = "Access restricted";
+};
+
+console.log(message);
