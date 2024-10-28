06748e701a5663df6c9abb64e772914d1ed5cd88
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-28 15:21:17 +0100

    add comments

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 console.log("Hello node js");
 
+// salvo la variabile d'ambiente
 const pin = process.env.PIN;
 console.log(pin);
 
+// salvo l'argomento passato 
 const arg = process.argv[2]
 console.log(arg);
 
 let message = "";
 
-if (arg == "admin" && pin == 666) {
+// verifico l'identitá e il codice di accesso
+if (arg === "admin" && Number(pin) === 666) {
     message = "Welcome adming";
 } else {
     message = "Access restricted";
