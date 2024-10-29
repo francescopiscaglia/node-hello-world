85d410290caa8e3812b99a783aa56aa17040672e
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-29 09:56:00 +0100

    add verification

--- main.js
+++ main.js
@@ -8,11 +8,21 @@ console.log(pin);
 const arg = process.argv[2]
 console.log(arg);
 
+if (!pin) {
+    console.log("You must add a PIN variable");
+    process.exit(1);
+};
+
+if (!arg) {
+    console.log("You must add a user");
+    process.exit(1);
+};
+
 let message = "";
 
 // verifico l'identitá e il codice di accesso
 if (arg === "admin" && Number(pin) === 666) {
-    message = "Welcome adming";
+    message = "Welcome admin";
 } else {
     message = "Access restricted";
 };
